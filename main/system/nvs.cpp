54dc4d4a4ee1bc25ef2002b7cc8153ebd7db2869
Shuichi Takano <shuichi.takano@gmail.com>
2020-11-29 15:31:22 +0900

    NVSに設定を保存するようにした

--- main/system/nvs.cpp
+++ main/system/nvs.cpp
@@ -6,6 +6,7 @@
 #include "nvs.h"
 #include <nvs.h>
 #include <nvs_flash.h>
+#include <vector>
 
 namespace sys
 {
@@ -33,6 +34,122 @@ initializeNVS()
     }
     initialized = true;
     return true;
+}
+
+////////////////
+
+NVSHandler::NVSHandler(const char* name, bool writable)
+{
+    initializeNVS();
+    if (nvs_open(name, writable ? NVS_READWRITE : NVS_READONLY, &handle_) ==
+        ESP_OK)
+    {
+        enabled_ = true;
+    }
+}
+
+NVSHandler::~NVSHandler()
+{
+    if (enabled_)
+    {
+        nvs_close(handle_);
+    }
+}
+
+std::experimental::optional<bool>
+NVSHandler::getBool(const char* key)
+{
+    uint8_t v;
+    if (enabled_ && ESP_OK == nvs_get_u8(handle_, key, &v))
+    {
+        return v;
+    }
+    return {};
+}
+
+std::experimental::optional<int32_t>
+NVSHandler::getInt(const char* key)
+{
+    int32_t v;
+    if (enabled_ && ESP_OK == nvs_get_i32(handle_, key, &v))
+    {
+        return v;
+    }
+    return {};
+}
+
+std::experimental::optional<uint32_t>
+NVSHandler::getUInt(const char* key)
+{
+    uint32_t v;
+    if (enabled_ && ESP_OK == nvs_get_u32(handle_, key, &v))
+    {
+        return v;
+    }
+    return {};
+}
+
+std::experimental::optional<float>
+NVSHandler::getFloat(const char* key)
+{
+    float v;
+    if (enabled_ &&
+        ESP_OK == nvs_get_u32(handle_, key, reinterpret_cast<uint32_t*>(&v)))
+    {
+        return v;
+    }
+    return {};
+}
+
+std::experimental::optional<std::string>
+NVSHandler::getString(const char* key)
+{
+    size_t size;
+    if (enabled_ && ESP_OK == nvs_get_str(handle_, key, nullptr, &size))
+    {
+        std::vector<char> buffer(size);
+        if (ESP_OK == nvs_get_str(handle_, key, buffer.data(), &size))
+        {
+            return {buffer.data()};
+        }
+    }
+    return {};
+}
+
+bool
+NVSHandler::setBool(const char* key, bool v)
+{
+    return enabled_ && ESP_OK == nvs_set_u8(handle_, key, v ? 1 : 0);
+}
+
+bool
+NVSHandler::setInt(const char* key, int32_t v)
+{
+    return enabled_ && ESP_OK == nvs_set_i32(handle_, key, v);
+}
+
+bool
+NVSHandler::setUInt(const char* key, uint32_t v)
+{
+    return enabled_ && ESP_OK == nvs_set_u32(handle_, key, v);
+}
+
+bool
+NVSHandler::setFloat(const char* key, float v)
+{
+    union
+    {
+        float f_;
+        uint32_t i_;
+    } u;
+    u.f_ = v;
+    return enabled_ && ESP_OK == nvs_set_u32(handle_, key, u.i_);
+}
+
+bool
+NVSHandler::setString(const char* key, const char* str)
+{
+    return enabled_ && ESP_OK == nvs_set_str(handle_, key, str);
 }
 
 } // namespace sys
--- main/system/nvs.h
+++ main/system/nvs.h
@@ -5,11 +5,42 @@
 #ifndef CEFCD123_2133_F0D1_26D7_187B66DB28EC
 #define CEFCD123_2133_F0D1_26D7_187B66DB28EC
 
+#include <experimental/optional>
+#include <stdint.h>
+#include <string>
+
 namespace sys
 {
 
 bool initializeNVS();
 
-}
+class NVSHandler
+{
+    bool enabled_    = false;
+    uint32_t handle_ = 0;
+
+public:
+    NVSHandler(const char* name, bool writable);
+    ~NVSHandler();
+    explicit operator bool() const { return enabled_; }
+
+    std::experimental::optional<bool> getBool(const char* key);
+    std::experimental::optional<int32_t> getInt(const char* key);
+    std::experimental::optional<uint32_t> getUInt(const char* key);
+    std::experimental::optional<float> getFloat(const char* key);
+    std::experimental::optional<std::string> getString(const char* key);
+
+    bool setBool(const char* key, bool v);
+    bool setInt(const char* key, int32_t v);
+    bool setUInt(const char* key, uint32_t v);
+    bool setFloat(const char* key, float v);
+    bool setString(const char* key, const char* str);
+
+private:
+    NVSHandler(const NVSHandler&) = delete;
+    NVSHandler& operator=(const NVSHandler&) = delete;
+};
+
+} // namespace sys
 
 #endif /* CEFCD123_2133_F0D1_26D7_187B66DB28EC */
